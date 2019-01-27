f2f6a1da7d0002d8b4ee0d2010658939d51d0eee
Nikita Rogalenko <nikitalr@mail.ru>
2019-01-27 14:35:37 +0300

    front-end: detail change interface + storage

--- src/main/webapp/WEB-INF/scripts/GarageScript.js
+++ src/main/webapp/WEB-INF/scripts/GarageScript.js
@@ -3,16 +3,28 @@ window.onload = function (ev) {
     carsArea = document.getElementById("carsZone");
     storArea = document.getElementById("storage");
     filterArea = document.getElementById("filter");
+    changesArea = document.getElementById("changes");
 };
 
 function chooseCars() {
     carsArea.hidden = false;
     storArea.hidden = true;
     filterArea.hidden = true;
+    changesArea.hidden = true;
 }
 
 function chooseStore() {
     carsArea.hidden = true;
     storArea.hidden = false;
     filterArea.hidden = false;
+    changesArea.hidden = true;
+}
+
+function chooseChange() {
+
+    carsArea.hidden = true;
+    storArea.hidden = true;
+    filterArea.hidden = true;
+    changesArea.hidden = false;
+
 }
